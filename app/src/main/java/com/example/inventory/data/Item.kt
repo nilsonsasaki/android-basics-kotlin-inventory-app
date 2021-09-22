6491d59ec8cca01efa3cbae43285028385ee534f
nilsonsasaki <nilsonsasaki@gmail.com>
2021-09-22 14:13:48 -0300

    created data class Item

---
+++ app/src/main/java/com/example/inventory/data/Item.kt
@@ -0,0 +1,17 @@
+package com.example.inventory.data
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "item")
+data class Item(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    @ColumnInfo
+    val itemName: String,
+    @ColumnInfo
+    val itemPrice: Double,
+    @ColumnInfo
+    val quantityInStock: Int
+)
