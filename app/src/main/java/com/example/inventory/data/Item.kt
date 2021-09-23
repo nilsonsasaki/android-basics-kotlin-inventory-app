e4560b13202ac7ed6acc2fa85f74a0c8291bdc26
nilsonsasaki <nilsonsasaki@gmail.com>
2021-09-23 18:57:13 -0300

    added getFormattedPrice

--- app/src/main/java/com/example/inventory/data/Item.kt
+++ app/src/main/java/com/example/inventory/data/Item.kt
@@ -3,6 +3,7 @@ package com.example.inventory.data
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import java.text.NumberFormat
 
 @Entity(tableName = "item")
 data class Item(
@@ -14,4 +15,7 @@ data class Item(
     val itemPrice: Double,
     @ColumnInfo
     val quantityInStock: Int
+
 )
+fun Item.getFormattedPrice(): String =
+    NumberFormat.getCurrencyInstance().format(itemPrice)
